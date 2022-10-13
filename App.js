4973390cc12b6161994c82f287418a56b4dea862
rodolfomori <rod_dolpho@hotmail.com>
2022-10-13 15:13:42 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { WebView } from 'react-native-webview'
+import { StatusBar } from 'react-native'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <>
+    <StatusBar barStyle="dark-content" backgroundColor="transparent"/>
+      <WebView
+        source={{ uri: 'https://app.devclub.com.br/' }}
+        style={{ marginTop: 45 }}
+      />
+    </>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
